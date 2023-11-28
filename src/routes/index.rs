5b6b4178891744e334a9c895f69c2e27820a779f
BViganotti <viganotti.exe@gmail.com>
2023-11-28 14:27:01 +0100

    added buttons tocontrol fridge and dehumidificator

--- src/routes/index.rs
+++ src/routes/index.rs
@@ -2,6 +2,8 @@ use crate::AccessSharedData;
 use actix_web::{http::header::ContentType, web, HttpResponse};
 use time::format_description;
 
+use super::relay_control::change_fridge_status;
+
 fn color_status(status: bool) -> String {
     if status != true {
         "<span style=\"color: #ff0000;\">OFF</span>".to_owned()
@@ -46,7 +48,14 @@ pub async fn index(common_data: web::Data<AccessSharedData>) -> HttpResponse {
     <li>last dehumidifier turn OFF time: </li>
      <li><strong>{}</strong></li>
     </ul>
-    <p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <form action=\"/change_fridge_status\" method=\"post\">
+        <button name=\"fridge_status\" value=\"fridge_status\">Change fridge status</button>
+    </form>
+    <form action=\"/change_dehumidifier_status\" method=\"post\">
+        <button name=\"dehumidifier_status\" value=\"dehumidifier_status\">Change dehumidifier status</button>
+    </form>
+    <p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p>
     <p><strong>Copyright DIVIGA 2023-Eternity</strong></p></body></html>",
         &common_data.average_temp().to_string(),
         &common_data.average_humidity().to_string(),
